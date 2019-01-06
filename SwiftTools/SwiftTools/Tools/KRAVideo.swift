27684558d622f4bf9cb8533da1696aaaba3892be
sixGod <z.o.e@outlook.com>
2019-01-06 19:11:21 +0800

    commit: 新增给视频添加水印功能

--- SwiftTools/SwiftTools/Tools/KRAVideo.swift
+++ SwiftTools/SwiftTools/Tools/KRAVideo.swift
@@ -32,3 +32,94 @@ class KRAVideo {
         return thumb
     }
 }
+
+
+extension URL {
+    
+    func addWaterMark(text: String, outFileName: String, completionHandler: @escaping (URL) -> Void) {
+        if !self.isFileURL {
+            debugPrint(self.absoluteString + " not is fileURL")
+            return
+        }
+        let videoAsset = AVURLAsset(url: self, options: [AVURLAssetPreferPreciseDurationAndTimingKey:true])
+        let mixComposition = AVMutableComposition()
+        let videoTrack = mixComposition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid)
+        let audioTrack = mixComposition.addMutableTrack(withMediaType: .audio, preferredTrackID: kCMPersistentTrackID_Invalid)
+        
+        guard let assetVideoTrack = videoAsset.tracks(withMediaType: .video).first else {
+            return print("assetVideoTrack nil")
+        }
+        
+        guard let assetAudioTrack = videoAsset.tracks(withMediaType: .audio).first else {
+            return print("assetAudioTrack nil")
+        }
+        
+        let endTime = assetVideoTrack.asset?.duration
+        do {
+            try videoTrack?.insertTimeRange(CMTimeRange(start: .zero, duration: endTime!), of: assetVideoTrack, at: .zero)
+            videoTrack?.preferredTransform = assetVideoTrack.preferredTransform
+            try audioTrack?.insertTimeRange(CMTimeRange(start: .zero, duration: endTime!), of: assetAudioTrack, at: .zero)
+            
+            let outPath = FileManager.urlFor(directory: .cachesDirectory) + outFileName
+            let outURL = URL(string: outPath)!
+            print(outURL.absoluteString)
+            
+            let result = FileManager.default.fileExists(atPath: outURL.path)
+            if result {
+                try FileManager.default.removeItem(atPath: outURL.path)
+                print("remove ok")
+            }
+            
+            
+            let videoSize = videoTrack!.naturalSize
+            let parentLayer = CALayer()
+            let videoLayer = CALayer()
+            parentLayer.frame = CGRect(origin: .zero, size: videoSize)
+            parentLayer.isGeometryFlipped = true
+            videoLayer.frame = parentLayer.frame
+            parentLayer.addSublayer(videoLayer)
+            
+            //            let watermarkLayer = CALayer()
+            //            watermarkLayer.contents = UIImage(named: "watermark")?.cgImage
+            //            watermarkLayer.frame = CGRect(x: 50, y: 100, width: 50, height: 50)
+            //            parentLayer.addSublayer(watermarkLayer)
+            
+            let textLayer = CATextLayer()
+            textLayer.string = text
+            textLayer.alignmentMode = .left
+            textLayer.foregroundColor = UIColor.white.cgColor
+            textLayer.backgroundColor = UIColor.clear.cgColor
+            textLayer.fontSize = 24
+            textLayer.frame = CGRect(x: 80, y: 100, width: 150, height: 100)
+            parentLayer.addSublayer(textLayer)
+            
+            
+            let videoComp = AVMutableVideoComposition()
+            videoComp.renderSize = videoSize
+            videoComp.frameDuration = CMTime(value: 1, timescale: 30)
+            videoComp.animationTool = AVVideoCompositionCoreAnimationTool(postProcessingAsVideoLayer: videoLayer, in: parentLayer)
+            
+            let instruction = AVMutableVideoCompositionInstruction()
+            instruction.timeRange = CMTimeRange(start: .zero, duration: endTime!)
+            let layerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack!)
+            instruction.layerInstructions = [layerInstruction]
+            videoComp.instructions = [instruction]
+            
+            let exporter = AVAssetExportSession(asset: mixComposition, presetName: AVAssetExportPresetHighestQuality)
+            exporter?.outputURL = outURL
+            exporter?.outputFileType = AVFileType.mov
+            exporter?.shouldOptimizeForNetworkUse = true
+            exporter?.videoComposition = videoComp
+            exporter?.exportAsynchronously(completionHandler: {
+                print(Thread.current)
+                DispatchQueue.main.async {
+                    completionHandler(outURL)
+                }
+            })
+            
+        } catch  {
+            print(error)
+        }
+        
+    }
+}
--- SwiftTools/SwiftTools/ViewController.swift
+++ SwiftTools/SwiftTools/ViewController.swift
@@ -9,13 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var test:UILabel?
     override func viewDidLoad() {
         super.viewDidLoad()
         let btn = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
             .addSelector(action: #selector(hello), for: .touchDown)
         btn.backgroundColor = .red
         self.view.addSubview(btn)
+        
+        let l = UILabel(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        l.text = "AAAAAAAA"
+//        l.font = UIFont.systemFont(ofSize: 15)
+        self.view.addSubview(l)
+        
+        test = l
+        
+        print(UIScreen.main.scale)
+        print(UIScreen.main.nativeScale)
+        print(UIScreen.main.bounds)
+        print(UIScreen.main.coordinateSpace)
+
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(test?.font)
+        
     }
     @objc func hello() {
         print("---------->> \(self).\(#function)<<---------")
