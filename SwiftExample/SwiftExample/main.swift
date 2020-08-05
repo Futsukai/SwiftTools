ff3f552d7a455cf825df5632b25cf6d64854329f
Erwin <z.o.e@outlook.com>
2020-08-05 15:56:32 +0800

    commit: 初始化样例工程

---
+++ SwiftExample/SwiftExample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftExample
+//
+//  Created by SuperMario on 2020/8/5.
+//  Copyright © 2020 SuperMario. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
